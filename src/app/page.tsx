ea655d236f58d8119e32f4e3605e306064cae163
Lucas <Lucas_Ferreira2019@outlook.com>
2024-01-31 16:06:58 -0300

    mapeando a api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,33 @@
-const page = ()=>{
+'use client'
+import { Product } from "../types/Product"
+
+const getProducts = async()=>{
+  const res = await fetch('https://fakestoreapi.com/products')
+
+  if (!res.ok) {
+    
+    throw new Error('Failed to fetch data')
+  }
+ 
+  return res.json()
+}
+
+
+
+const page = async()=>{
+
+  const products = await getProducts();
+  console.log(products);
+  
   return(
     <div className="container pt-8 px-8 mx-auto xl:px-0">
       <div className="grid grid-cols-1 sm:grid-cols-2 mg:grid-cols-3 lg:grid-cols-4 xl:gap-6">
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-          produto 1
+      {products.map((product:Product)=>(
+        <div key={product.id}>
+          <img className="w-10" src={product.image} alt="" />
+          <h1>{product.title}</h1>
         </div>
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-          produto 1
-        </div>
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-          produto 1
-        </div>
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden">
-          produto 1
-        </div>
+      ))}
       </div>
     </div>
   )
